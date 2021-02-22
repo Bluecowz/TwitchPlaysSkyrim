cc1feb1052a1920b801be5f71fd3b88b8f5b964d
Mike Gallant <taurenmike@gmail.com>
2021-02-22 16:17:23 -0600

    added comamnds enum

--- main.py
+++ main.py
@@ -5,15 +5,16 @@ import sys
 import getopt
 import os
 import time
+from Commands import Commands
 from enum import Enum
 from pynput.keyboard import Key, Controller as KeyboardController
 from pynput.mouse import Button, Controller as MouseController, Listener
 
 ### TODOS
-# TODO Enum for commands 
 # TODO logging 
+# TODO make pretty readme for commands
+# TODO Enum for commands 
 # TODO Filter none command messages
-# TODO Remove magic numbers for mouse
 # TODO Have long and short versions for commands
 ###
 
@@ -28,8 +29,7 @@ logging.basicConfig(level=logging.DEBUG,
 def on_move(x,y):
     print("print('Pointer moved to {0}".format((x,y)))
 
-
-def ParseCommand(message):
+def ParseCommand(message,username, channel):
     MOUSE_STEP=100
     message = message.replace('\r', '')
     commands = re.search('([a-zA-Z]*)([0-9]*)',message)
@@ -37,147 +37,147 @@ def ParseCommand(message):
         return
     command, value = commands.groups()
     print('Parsing: ' + command)
-    if command == 'rm': # Click Right Mouse
+    if command == Commands.RIGHT_MOUSE: # Click Right Mouse
         mouse.press(Button.right)
         time.sleep(0.3)
         mouse.release(Button.right)
-    elif command == 'hrm': # Hold Right Mouse
+    elif command == Commands.HOLD_RIGHT_MOUSE: # Hold Right Mouse
         mouse.press(Button.right)
-    elif command == 'rrm': # Release Right Mouse
+    elif command == Commands.RELEASE_RIGHT_MOUSE: # Release Right Mouse
         mouse.release(Button.right)
-    elif command == 'lm': # Click Left Mouse
+    elif command == Commands.LEFT_MOUSE: # Click Left Mouse
         mouse.press(Button.left)
         time.sleep(0.3)
         mouse.release(Button.left)
-    elif command == 'hlm': # Hold Left Mouse
+    elif command == Commands.HOLD_LEFT_MOUSE: # Hold Left Mouse
         mouse.press(Button.left)
-    elif command == 'rlm': # Release Left Mouse
+    elif command == Commands.RELEASE_LEFT_MOUSE: # Release Left Mouse
         mouse.release(Button.left)
-    elif command == 'mup':
+    elif command == Commands.MOUSE_UP: # Move Mouse Up
         mouse.position = (1,1)
         if value != '':
             mouse.move(0,-int(value))
         else:
             mouse.move(0,-MOUSE_STEP)
-    elif command == 'mdown':
+    elif command == Commands.MOUSE_DOWN: # Move Mouse Down
         mouse.position = (1,1)
         if value != '':
             mouse.move(0,int(value))
         else:
             mouse.move(0,MOUSE_STEP)
-    elif command == 'mleft':
+    elif command == Commands.MOUSE_LEFT: # Move Mouse Left
         mouse.position = (1,1)
         if value != '':
             mouse.move(-int(value), 0)
         else:
             mouse.move(-MOUSE_STEP, 0)
-    elif command == 'mright':
+    elif command == Commands.MOUSE_RIGHT: # Move Mouse Right
         mouse.position = (1,1)
         if value != '':
             mouse.move(int(value), 0)
         else:    
             mouse.move(MOUSE_STEP,0)
-    elif command == 'halt': # Hold Alt
+    elif command == Commands.HOLD_ALT: # Hold Alt
         keyboard.press(Key.alt)
-    elif command == 'ralt': # Release Alt
+    elif command == Commands.RELEASE_ALT: # Release Alt
         keyboard.release(Key.alt)
-    elif command == "Z": # Press Z
+    elif command == Commands.PRESS_Z: # Press Z
         keyboard.press('z')
         keyboard.release('z')
-    elif command == 'w': # Press W
+    elif command == Commands.PRESS_W: # Press W
         keyboard.press('w')
         keyboard.release('w')
-    elif command == 's': # Press S
+    elif command == Commands.PRESS_S: # Press S
         keyboard.press('s')
         keyboard.release('s')
-    elif command == 'a': # Press A
+    elif command == Commands.PRESS_A: # Press A
         keyboard.press('a')
         keyboard.release('a')
-    elif command == 'd': # Press D
+    elif command == Commands.PRESS_D: # Press D
         keyboard.press('d')
         keyboard.release('d')
-    elif command == 'hw': # Hold W
+    elif command == Commands.HOLD_W: # Hold W
         keyboard.press('w')
-    elif command == 'ha': # Hold A
+    elif command == Commands.HOLD_A: # Hold A
         keyboard.press('a')
-    elif command == 'hs': # Hold S
+    elif command == Commands.HOLD_S: # Hold S
         keyboard.press('s')
-    elif command == 'hd': # Hold D
+    elif command == Commands.HOLD_D: # Hold D
         keyboard.press('d')
-    elif command == 'rw': # Release W
+    elif command == Commands.RELEASE_W: # Release W
         keyboard.release('w')
-    elif command == 'ra': # Release A
+    elif command == Commands.RELEASE_A: # Release A
         keyboard.release('a')
-    elif command == 'rs': # Release S
+    elif command == Commands.RELEASE_S: # Release S
         keyboard.release('s')
-    elif command == 'rd': # Release D
+    elif command == Commands.RELEASE_D: # Release D
         keyboard.release('d')
-    elif command == 'ctrl': # Press Ctrl
+    elif command == Commands.PRESS_CTRL: # Press Ctrl
         keyboard.press(Key.ctrl)
         keyboard.release(Key.ctrl)
-    elif command == 'f':  # Press F (to pay respect?)
+    elif command == Commands.PRESS_F:  # Press F (to pay respect?)
         keyboard.press('f')
         keyboard.release('f')
-    elif command == 'clock': # Press Caps Lock
+    elif command == Commands.PRESS_CAPS_LOCK: # Press Caps Lock
         keyboard.press(Key.caps_lock)
         keyboard.release(Key.caps_lock)
-    elif command == 't': # Press T
+    elif command == Commands.PRESS_T: # Press T
         keyboard.press('t')
         keyboard.release('t') 
-    elif command == 'j': # Press J
+    elif command == Commands.PRESS_J: # Press J
         keyboard.press('j')
         keyboard.release('j')
-    elif command == 'e': # Press E
+    elif command == Commands.PRESS_E: # Press E
         keyboard.press('e')
         keyboard.release('e')
-    elif command == 'tab': # Press Tab
+    elif command == Commands.PRESS_TAB: # Press Tab
         keyboard.press(Key.tab)
         keyboard.release(Key.tab)
-    elif command == 'r': # Press R
+    elif command == Commands.PRESS_R: # Press R
         keyboard.press('r')
         keyboard.release('r')
-    elif command == 'l': # Press L
+    elif command == Commands.PRESS_L: # Press L
         keyboard.press('l')
         keyboard.release('l')
-    elif command == 'm': # Press M
+    elif command == Commands.PRESS_M: # Press M
         keyboard.press('m')
         keyboard.release('m')
-    elif command == 'space': # Press Space
+    elif command == Commands.PRESS_SPACE: # Press Space
         keyboard.press(Key.space)
         keyboard.release(Key.space)
-    elif command == 'he': # Hold E
+    elif command == Commands.HOLD_E: # Hold E
         keyboard.press('e')
-    elif command == 're': # Release E
+    elif command == Commands.RELEASE_E: # Release E
         keyboard.release('e')
-    elif command == 'q': # Press Q
+    elif command == Commands.PRESS_Q: # Press Q
         keyboard.press('q')
         keyboard.release('q')
-    elif command == 'esc': # Press Escape
+    elif command == Commands.ESC: # Press Escape
         keyboard.press(Key.esc)
         keyboard.release(Key.esc)
-    elif command == 'f5': # Press F5
+    elif command == Commands.F5: # Press F5
         keyboard.press(Key.f5)
         keyboard.release(Key.f5)
-    elif command == 'up': # Press Up
+    elif command == Commands.UP_ARROW: # Press Up
         keyboard.press(Key.up)
         keyboard.release(Key.up)
-    elif command == 'down': # Press Down
+    elif command == Commands.DOWN_ARROW: # Press Down
         keyboard.press(Key.down)
         keyboard.press(Key.down)
-    elif command == 'left': # Press Left
+    elif command == Commands.LEFT_ARROW: # Press Left
         keyboard.press(Key.left)
         keyboard.release(Key.left)
-    elif command == 'right': # Press Right
+    elif command == Commands.RIGHT_ARROW: # Press Right
         keyboard.press(Key.right)
         keyboard.release(Key.right)
-    elif command == 'hshift': # Hold Shift
+    elif command == Commands.HOLD_SHIFT: # Hold Shift
         keyboard.press(Key.shift_l)
-    elif command == 'rshift': # Release Shift
+    elif command == Commands.RELEASE_SHIFT: # Release Shift
         keyboard.release(Key.shift_l)
-    elif command == 'enter': # Press Enter
+    elif command == Commands.ENTER: # Press Enter
         keyboard.press(Key.enter)
         keyboard.release(Key.enter)
-    elif command == 'reset_buttons': # Resets on Held Buttons
+    elif command == Commands.RESET_BUTTONS: # Resets on Held Buttons
         mouse.release(Mouse.right)
         mouse.release(Mouse.left)
         keyboard.release('w')
@@ -188,10 +188,9 @@ def ParseCommand(message):
         keyboard.release('e')
         keyboard.release(Key.shift_l)
         keyboard.release(Key.alt)
-    elif command == 'moo': #Test Command
+    elif command == Commands.MOO: #Test Command
         print('Said The Blue Cow')
-    else:
-        print('Not A Recognized command: ' + command)
+    logging.debug("{}:{}:{}".format(channel,username,message))
 
 def main(argv):
     server = 'irc.chat.twitch.tv'
@@ -234,7 +233,7 @@ def main(argv):
                 if fmtre is not None:
                     username, channel, message = fmtre.groups()
                     # print(f"Channel: {channel} \nUsername: {username} \nMessage: {message}")
-                    ParseCommand(message)
+                    ParseCommand(message, username, channel)
     except KeyboardInterrupt:
         sock.close()
         print('Socket Closed')
