3c35b6e5671d6277a954211ac1fb50fcae4fc285
Mike Gallant <taurenmike@gmail.com>
2021-02-25 21:11:18 -0600

    embarrassing typos

--- main.py
+++ main.py
@@ -42,28 +42,28 @@ def click(key):
 
 def mouse_down(y):
     mouse.position = (1,1)
-    if y != '' && y > 0 && y <= 3000:
+    if y != '' and y > 0 and y <= 3000:
         mouse.move(0,int(y))
     else:
         mouse.move(0,MOUSE_STEP)
 
 def mouse_up(y):
     mouse.position = (1,1)
-    if y != '' && y > 0 && y <= 3000:
+    if y != '' and y > 0 and y <= 3000:
         mouse.move(0,-int(y))
     else:
         mouse.move(0,-MOUSE_STEP)
 
 def mouse_right(x):
     mouse.position = (1,1)
-    if x != '' && x > 0 && x <= 4000:
+    if x != '' and x > 0 and x <= 4000:
         mouse.move(int(x), 0)
     else:    
         mouse.move(MOUSE_STEP,0)
 
 def mouse_left(x):
     mouse.position = (1,1)
-    if x != '' && x > 0 && x <= 4000:
+    if x != '' and x > 0 and x <= 4000:
         mouse.move(-int(x), 0)
     else:
         mouse.move(-MOUSE_STEP, 0)
@@ -102,12 +102,12 @@ commands = [
     ('a', lambda: press_key('a')),
     ('d', lambda: press_key('d')),
     ('hw', lambda: keyboard.press('w')),
-    ('hs', lambda: keyboard.press('a')),
-    ('ha', lambda: keyboard.press('s')),
+    ('hs', lambda: keyboard.press('s')),
+    ('ha', lambda: keyboard.press('a')),
     ('hd', lambda: keyboard.press('d')),
     ('rw', lambda: keyboard.release('w')),
-    ('rs', lambda: keyboard.release('a')),
-    ('ra', lambda: keyboard.release('s')),
+    ('rs', lambda: keyboard.release('s')),
+    ('ra', lambda: keyboard.release('a')),
     ('rd', lambda: keyboard.release('d')),
     ('ctrl', lambda: press_key(Key.ctrl)),
     ('f', lambda: press_key('f')),
@@ -179,7 +179,7 @@ def main(argv):
                     username, channel, message = fmtre.groups()    
                     message = message.replace('\r', '')
                     # print(f"Channel: {channel} \nUsername: {username} \nMessage: {message}")
-                    msg = re.search('([a-zA-Z]+)[ ]?([0-9]*)',message)
+                    msg = re.search('([a-zA-Z_]+)[ ]?([0-9]*)',message)
                     if msg != None:
                         command, value = msg.groups()
                         for c in commands:
